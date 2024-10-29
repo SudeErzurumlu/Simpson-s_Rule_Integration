1fa0377c4c7481ef4c7384dbb4012831e33d0b88
Sude <151455137+SudeErzurumlu@users.noreply.github.com>
2024-10-29 23:29:55 +0300

    Create simpsons_rule.h

---
+++ C/include/simpsons_rule.h
@@ -0,0 +1,10 @@
+// simpsons_rule.h
+// Header file for Simpson's Rule Integration in C
+
+#ifndef SIMPSONS_RULE_H
+#define SIMPSONS_RULE_H
+
+// Function prototype for Simpson's Rule integration
+double simpsonsRule(double (*f)(double), double a, double b, int n);
+
+#endif // SIMPSONS_RULE_H
